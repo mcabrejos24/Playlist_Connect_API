3e3195b0cd20b2ac572c6228476725efeaa3a162
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-02-23 23:56:52 -0500

    adds playlist pairs view

--- playlist_connect_api/views.py
+++ playlist_connect_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from playlist_connect_api import serializers
+from rest_framework import viewsets
 
-# Create your views here.
+from playlist_connect_api import serializers
+from playlist_connect_api import models
+
+class PlaylistPairsViewSet(viewsets.ModelViewSet):
+    """Handles creating playlist pairs"""
+    serializer_class = serializers.PlaylistPairsSerializer
+    queryset = models.PlaylistPairs.objects.all();
+    
