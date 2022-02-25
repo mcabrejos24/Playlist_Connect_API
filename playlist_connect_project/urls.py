6ecf3477450791f7169866629e8d3015966b82bd
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-02-25 17:54:43 -0500

    updates api path

--- playlist_connect_project/urls.py
+++ playlist_connect_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('playlist_connect.urls'))
+    path('api/', include('playlist_connect_api.urls'))
 ]
