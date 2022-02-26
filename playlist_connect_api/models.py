b4cd499e5aa25589621e0cc53f5fae9f02cef40b
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-02-26 15:18:38 -0500

    updates playlistpairs model to use playlist IDs

--- playlist_connect_api/models.py
+++ playlist_connect_api/models.py
@@ -7,19 +7,24 @@ from django.db import models
 
 class PlaylistPairs(models.Model):
     """Database Model for Playlist Pairs"""
-    apple_music_token = models.CharField(max_length=128)
-    spotify_music_token = models.CharField(max_length=128)
-    apple_playlist_name = models.CharField(max_length=255)
-    spotify_playlist_name = models.CharField(max_length=255)
+    apple_token_1 = models.CharField(max_length=128)
+    apple_token_2 = models.CharField(max_length=128)
+    apple_token_3 = models.CharField(max_length=128)
+    spotify_token_1 = models.CharField(max_length=128)
+    spotify_token_2 = models.CharField(max_length=128)
+    spotify_token_3 = models.CharField(max_length=128)
+    apple_playlist_id = models.CharField(max_length=255)
+    spotify_playlist_id = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
 
-    def get_apple_playlist_name(self):
+    def get_apple_playlist_id(self):
         """Retieve the Apple playlist name"""
-        return self.apple_playlist_name
+        return self.apple_playlist_id
 
-    def get_apple_playlist_name(self):
+    def get_apple_playlist_id(self):
         """Retieve the Spotify playlist name"""
-        return self.spotify_playlist_name
+        return self.spotify_playlist_id
 
     def __str__(self):
         """Return string representation of the PlaylistPairs object"""
-        return "apple-" + self.apple_playlist_name + "_" + "spotify-" + self.spotify_playlist_name
+        return "apple-" + self.apple_playlist_id + "_" + "spotify-" + self.spotify_playlist_id
