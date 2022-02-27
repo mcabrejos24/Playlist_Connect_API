15f24d708b367d6f2849aa429d493841d4cb4bc4
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-02-27 15:03:43 -0500

    completes the sync functions

--- playlist_connect_api/sync.py
+++ playlist_connect_api/sync.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import requests
 import base64
 import json
@@ -11,14 +12,14 @@ class StartSync():
 
     def get_apple_isrc(song_ids, header):
         url = 'https://api.music.apple.com/v1/catalog/us/songs/'
-        isrc = []
+        isrc = set()
         for id in song_ids:
             response = requests.get(url+id, headers=header);
             if not response:
                 print('Failed to get ISRC')
                 return
             response_json = json.loads(response.content.decode('utf8').replace("'", '"'))
-            isrc.append(response_json['data'][0]['attributes']['isrc'])
+            isrc.add(response_json['data'][0]['attributes']['isrc'])
 
 
         return isrc
@@ -32,17 +33,18 @@ class StartSync():
             return
         response_json = json.loads(response.content.decode('utf8').replace("'", '"'))
 
-        songs_isrc = []
+        songs_isrc = set()
         if service == 'spotify':
             for song in response_json['items']:
                 if song['track'] and song['track']['external_ids'] and song['track']['external_ids']['isrc']:
-                    songs_isrc.append(song['track']['external_ids']['isrc'])
+                    # songs_isrc.append(song['track']['external_ids']['isrc'])
+                    songs_isrc.add(song['track']['external_ids']['isrc'])
             return songs_isrc
         elif service == 'apple':
-            apple_song_ids = []
+            apple_song_ids = set()
             for song in response_json['data']:
                 if song['attributes'] and song['attributes']['playParams'] and song['attributes']['playParams']['catalogId']:
-                    apple_song_ids.append(song['attributes']['playParams']['catalogId'])
+                    apple_song_ids.add(song['attributes']['playParams']['catalogId'])
                 songs_isrc = StartSync.get_apple_isrc(apple_song_ids, header)
             return songs_isrc
         return ['FAILED TO GET ANY SONG ISRC CODES']
@@ -54,6 +56,61 @@ class StartSync():
         # if duplicates then maybe oh well, but lets try it
         # will grab songs by either a specific song code
         # or by finding, artist, song name, and album
+
+    @staticmethod
+    def api_get(service, url, header):
+        res = requests.get(url, headers=header);
+        if not res:
+            print(f'Failed in making get call, url: {url}')
+            return
+        return json.loads(res.content.decode('utf8').replace("'", '"'))
+
+    @staticmethod
+    def api_post(service, url, header, payload):
+        
+        result = requests.post(url, data=payload, headers=header)
+
+        return result
+
+    @staticmethod
+    def add_playlist_songs_to(service, isrcs, header, playlist_id):
+        if service == 'apple':
+            song_ids = []
+            for isrc in isrcs:
+                url = f'https://api.music.apple.com/v1/catalog/us/songs?filter[isrc]={isrc}'
+                response_json = StartSync.api_get(service, url, header)
+                if not response_json:
+                    print(f'Failed in making getting songs, url: {url}')
+                    return
+                if response_json['data'] and response_json['data'][0] and response_json['data'][0]['id']:
+                    # song_ids.append(response_json['data'][0]['id'])
+                    song_ids.append({"id": f"{response_json['data'][0]['id']}", "type": "songs"})
+            
+            url = f'https://api.music.apple.com/v1/me/library/playlists/{playlist_id}/tracks'
+            payload = f'{{"data": {song_ids}}}'
+            payload = payload.replace("'", '"')
+            res = StartSync.api_post(service, url, header, payload)
+            return res
+        elif service == 'spotify':
+            uris = []
+            for isrc in isrcs:
+                url = f'https://api.spotify.com/v1/search?type=track&q=isrc:{isrc}'
+                print('can we at least get this one')
+                response_json = StartSync.api_get(service, url, header)
+                if not response_json:
+                    print(f'Failed in making getting uris, url: {url}')
+                    return
+                if response_json['tracks'] and response_json['tracks']['items'] and response_json['tracks']['items'][0] and response_json['tracks']['items'][0]['uri']:
+                    uris.append(response_json['tracks']['items'][0]['uri'])
+
+            url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+            payload = f'{{"uris": {uris}}}'
+            payload = payload.replace("'", '"')
+
+            res = StartSync.api_post(service, url, header, payload)
+            print(res)
+            return res
+        return
 
     @staticmethod
     def sync(playlistPairObject):
@@ -72,23 +129,13 @@ class StartSync():
         }
         apple_songs = StartSync.get_playlist_songs('apple', StartSync.apple_url, apple_header, playlistPairObject.apple_playlist_id)
 
-        # add both lists to one set, to erase overlappings, then add to each and just don't add what's already there
+        print(spotify_auth)
+        print(' ')
+        print(apple_auth)
 
-        # make both sets, then from one remove overlappings, then add 
+        apple_successful = StartSync.add_playlist_songs_to('apple', spotify_songs-apple_songs, apple_header, playlistPairObject.apple_playlist_id)
+        spotify_successful = StartSync.add_playlist_songs_to('spotify', apple_songs-spotify_songs, spotify_header, playlistPairObject.spotify_playlist_id)
 
-        # get intersection (O(smallest list))
-        # remove intersection from list 1 and then remaining send to opp service (O(intersection )) + O(length of remaining list)
-        # do same for list 2 (O(intersection )) + O(length of remaining list)
-
-        # loop through songs to add and check if other list contains (O(list))
-        # same for other
-
-        # 12345  23456
-
-        # 5 1 1
-        # 5 5
-
-        print(spotify_songs)
-        print(apple_songs)
-
-        return;
+        print(spotify_successful)
+        print(apple_successful)
+        return apple_successful and spotify_successful
