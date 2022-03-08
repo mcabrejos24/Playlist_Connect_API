1a2ea5be196f1cb790d6ddc49606f5789dfff207
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-08 13:54:54 -0500

    adds static root and dirs to settings.py

--- playlist_connect_project/settings.py
+++ playlist_connect_project/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 CORS_ALLOW_ALL_ORIGINS = False
 
