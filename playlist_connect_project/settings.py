cc283510e64734077e73681823d18f75e698cf81
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-08 13:58:30 -0500

    removes static from settings.py

--- playlist_connect_project/settings.py
+++ playlist_connect_project/settings.py
@@ -123,10 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+# if I were to use static files, then I would uncomment the line below
+# STATIC_URL = '/static/' 
 
 CORS_ALLOW_ALL_ORIGINS = False
 
