81ff4a6336ac4644cf494e1f63039b7307d3dd65
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-10 00:53:25 -0500

    sets up django-heroku library

--- playlist_connect_project/settings.py
+++ playlist_connect_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,6 @@ Q_CLUSTER = {
     'timeout': 60,
     'retry': 70,
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals(), allowed_hosts=False, staticfiles=False)
