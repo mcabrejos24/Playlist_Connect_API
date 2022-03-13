0f01b9adaa14e600976d13c9686ae785440651ae
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-13 00:04:03 -0500

    updates serlializer

--- playlist_connect_api/serializers.py
+++ playlist_connect_api/serializers.py
@@ -7,4 +7,5 @@ class PlaylistPairsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PlaylistPairs
         fields = ('id', 'apple_token_1', 'apple_token_2', 'apple_token_3', 'spotify_token_1', 'spotify_token_2', 'spotify_token_3', 'spotify_refresh_1', 'spotify_refresh_2', 'apple_playlist_id', 'spotify_playlist_id')
-        # extra_kwargs = {'apple_music_token': {'read_only': True}, 'spotify_music_token': {'read_only': True}}
+        # disables access to update db via ui, remove for access in development
+        extra_kwargs = {'apple_token_1': {'read_only': True}, 'apple_token_2': {'read_only': True}, 'apple_token_3': {'read_only': True}, 'spotify_token_1': {'read_only': True}, 'spotify_token_2': {'read_only': True}, 'spotify_token_3': {'read_only': True}, 'spotify_refresh_1': {'read_only': True}, 'spotify_refresh_2': {'read_only': True}, 'apple_playlist_id': {'read_only': True}, 'spotify_playlist_id': {'read_only': True}}
