b7d6c445a1ecdb3410d625d0c3449edb86ebd3b0
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-13 01:39:04 -0500

    removes extra_kwargs from serizlier

--- playlist_connect_api/serializers.py
+++ playlist_connect_api/serializers.py
@@ -8,4 +8,3 @@ class PlaylistPairsSerializer(serializers.ModelSerializer):
         model = models.PlaylistPairs
         fields = ('id', 'apple_token_1', 'apple_token_2', 'apple_token_3', 'spotify_token_1', 'spotify_token_2', 'spotify_token_3', 'spotify_refresh_1', 'spotify_refresh_2', 'apple_playlist_id', 'spotify_playlist_id')
         # disables access to update db via ui, remove for access in development
-        extra_kwargs = {'apple_token_1': {'read_only': True}, 'apple_token_2': {'read_only': True}, 'apple_token_3': {'read_only': True}, 'spotify_token_1': {'read_only': True}, 'spotify_token_2': {'read_only': True}, 'spotify_token_3': {'read_only': True}, 'spotify_refresh_1': {'read_only': True}, 'spotify_refresh_2': {'read_only': True}, 'apple_playlist_id': {'read_only': True}, 'spotify_playlist_id': {'read_only': True}}
