1b5f18a2b8d65277c8b8e120f0b792e1f3910a81
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-14 01:24:28 -0400

    fixes manage.py lint again

--- manage.py
+++ manage.py
@@ -5,8 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", \
-        "playlist_connect_project.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", 
+        "playlist_connect_project.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
