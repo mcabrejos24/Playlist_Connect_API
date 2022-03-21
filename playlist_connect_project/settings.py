b924128a1c13d583db60128e8d107af10b447396
mcabrejos24 <manuelcabrejos97@gmail.com>
2022-03-20 22:51:56 -0400

    reverts more updates

--- playlist_connect_project/settings.py
+++ playlist_connect_project/settings.py
@@ -95,24 +95,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "postgres",
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASS"),
+        "USER": "postgres",
+        "PASSWORD": "password",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
-
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
 
 
 # Password validation
